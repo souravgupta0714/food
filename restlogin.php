2a3c923786613c56279783f0e8a99dad3decba7d
souravgupta0714 <84475378+souravgupta0714@users.noreply.github.com>
2021-05-20 11:53:29 +0530

    Update restlogin.php

--- restlogin.php
+++ restlogin.php
@@ -4,7 +4,7 @@
 	
 	session_start();
 	if(isset($_SESSION['resName']))
-		header('location: addMenuItem.php');
+		header('location: addmenuitem.php');
 	else if (isset($_SESSION['username']))
 		header('location: menu.php');
 	
