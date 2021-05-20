7462d1709356b92c8291f15c17ef6fa617764a16
souravgupta0714 <84475378+souravgupta0714@users.noreply.github.com>
2021-05-20 11:27:12 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -2,7 +2,7 @@
     $dbname = "heroku_47f78b9e3c68ec5";
     $username = "b22a18d5367ffd";
     $password = "f00a0e22";
-    $hostname = "localhost";
+    $hostname = "us-cdbr-east-03.cleardb.com";
 
     $dbc = mysqli_connect($hostname, $username, $password, $dbname);
 ?>
